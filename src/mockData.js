9538bfa68c24d544d747bdc1ca029132d9757289
SreejaBandari <bandarisri17@gmail.com>
2022-07-08 17:38:27 -0400

    First Commit

---
+++ src/mockData.js
@@ -0,0 +1,98 @@
+export const mockData = [
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 78,
+        transactionDt: "05-01-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 160,
+        transactionDt: "05-10-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 94,
+        transactionDt: "05-17-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 40,
+        transactionDt: "06-03-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 82,
+        transactionDt: "06-17-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 164,
+        transactionDt: "07-08-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 100,
+        transactionDt: "07-14-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 180,
+        transactionDt: "07-23-2022"
+    },
+    {
+        custid: 1,
+        name: "Sreeja",
+        amt: 420,
+        transactionDt: "07-27-2022"
+    },
+    {
+        custid: 2,
+        name: "Prashanth",
+        amt: 15,
+        transactionDt: "05-06-2022"
+    },
+    {
+        custid: 2,
+        name: "Prashanth",
+        amt: 45,
+        transactionDt: "05-21-2022"
+    },
+    {
+        custid: 2,
+        name: "Prashanth",
+        amt: 90,
+        transactionDt: "06-02-2022"
+    },
+    {
+        custid: 2,
+        name: "Prashanth",
+        amt: 105,
+        transactionDt: "06-17-2022"
+    },
+    {
+        custid: 2,
+        name: "Prashanth",
+        amt: 180,
+        transactionDt: "07-01-2022"
+    },
+    {
+        custid: 2,
+        name: "Prashanth",
+        amt: 240,
+        transactionDt: "07-17-2022"
+    },
+    {
+        custid: 3,
+        name: "Bandari",
+        amt: 160,
+        transactionDt: "06-19-2022"
+    }
+];
